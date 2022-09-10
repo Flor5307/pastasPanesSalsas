7c98e9e2a99d5ca0750b131abfa38c8b4fba7f35
Flor5307 <104154103+Flor5307@users.noreply.github.com>
2022-09-10 17:07:38 -0300

    navbar a medio hacer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./components/Navbar";
+import Navbar from "./components/Navbar";
+import "./components/Navbar.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar />
+        <h1>Mi titulo</h1>
+        <button>Click me</button>
       </header>
     </div>
   );
 }
 
-export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+/*import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";*/
+
+import React from "react";
+import { MenuItems } from "./MenuItems";
+import { Nav } from "react-bootstrap";
+
+export default function Navbar() {
+  return (
+    <Nav className="NavbarItems">
+      <h1 className="Navbar-logo">React</h1>
+      <div className="menu-icon"></div>
+      <ul>
+        {MenuItems.map((item, index) => {
+          return (
+            <li key={index}>
+              <a className={item.cName} href={item.url}>
+                {item.Title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </Nav>
+  );
+}
